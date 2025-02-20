37c7b91760d76dcc206f4199f036b3dd02aa8a08
BrakeZap <kyle.j.smith56@gmail.com>
2025-02-19 17:17:30 -0800

    Initial server implementation with axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/create", post(handle_post));
+    let addr = "0.0.0.0:3030";
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("Server running on {}", addr);
+    axum::serve::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hi from the partage server!"
+}
+#[derive(Deserialize)]
+struct RequestData {
+    id: String,
+    file: Vec<u8>,
+}
+
+#[derive(Serialize)]
+struct ResponseData {
+    response: String,
+}
+
+async fn handle_post(Json(payload): Json<RequestData>) -> Json<ResponseData> {
+    Json(ResponseData {
+        response: format!("Created file."),
+    })
 }
