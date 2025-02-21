f04be3cebbd69c2d076d7eaa905fffe4fbf36dee
BrakeZap <kyle.j.smith56@gmail.com>
2025-02-20 18:06:18 -0800

    Added file state and downloading functionality

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use axum::{
+    extract::State,
+    http::StatusCode,
     routing::{get, post},
     Json, Router,
 };
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
+use serde_json::json;
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() {
+    let files = Arc::new(Mutex::new(HashMap::new()));
+
     let app = Router::new()
         .route("/", get(root))
-        .route("/create", post(handle_post));
+        .route("/create", post(handle_post))
+        .route("/download/{id}", get(handle_download))
+        .with_state(files);
     let addr = "0.0.0.0:3030";
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     println!("Server running on {}", addr);
@@ -22,16 +32,53 @@ async fn root() -> &'static str {
 struct RequestData {
     id: String,
     file: Vec<u8>,
+    file_name: String,
     hash: Vec<u8>,
 }
 
-#[derive(Serialize)]
-struct ResponseData {
-    response: String,
+struct MemFile {
+    file: Vec<u8>,
+    file_name: String,
+    hash: Vec<u8>,
 }
 
-async fn handle_post(Json(payload): Json<RequestData>) -> Json<ResponseData> {
-    Json(ResponseData {
-        response: format!("Created file."),
-    })
+async fn handle_post(
+    State(state): State<Arc<Mutex<HashMap<String, MemFile>>>>,
+
+    Json(payload): Json<RequestData>,
+) -> axum::http::StatusCode {
+    println!("received post with id: {}\n", payload.id);
+
+    println!("length of file: {}", payload.file.len());
+
+    println!("length of hash: {}", payload.hash.len());
+
+    let mut map = state.lock().await;
+    map.insert(
+        payload.id,
+        MemFile {
+            file_name: payload.file_name,
+            file: payload.file,
+            hash: payload.hash,
+        },
+    );
+    StatusCode::OK
+}
+
+async fn handle_download(
+    State(state): State<Arc<Mutex<HashMap<String, MemFile>>>>,
+    axum::extract::Path(file_id): axum::extract::Path<String>,
+) -> Json<serde_json::Value> {
+    let map = state.lock().await;
+
+    if !map.contains_key(&file_id) {
+        return Json(json!(""));
+    }
+
+    let file_info = map.get(&file_id).unwrap();
+
+    let json =
+        json!({"file_name": file_info.file_name, "file": file_info.file, "hash": file_info.hash});
+
+    Json(json)
 }
